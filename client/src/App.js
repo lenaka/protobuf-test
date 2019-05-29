04209f195b1a84c51e4091cb88b385b9bf56ec93
Elena Kalashnikova <kalashnikovae@mail.ru>
2019-05-30 01:36:50 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Info } from "./component/info";
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+    </div>
+  );
+}
+
+export default App;
